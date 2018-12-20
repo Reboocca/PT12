8afbc924f33a9e256a20894c70b04d94b9141a77
Damian Tomasila <I406383@fhict.nl>
2018-12-20 15:30:44 +0100

    E

---
+++ Match-EventApp/Match-EventApp/Group.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Match_EventApp
+{
+    class Group
+    {
+        //list voor een groep
+        public List<int> group1 = new List<int>();
+
+        //test int
+        int test = 1;
+
+        public void GroupJoin()
+        {
+            //"profiel" toevoegen aan de list
+            group1.Add(test);
+        }
+    }
+}

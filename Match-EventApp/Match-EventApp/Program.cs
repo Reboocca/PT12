f427270d28d8d02d59020b8d0cae0dcf2a9bc660
Rick Peters <rickkert1999@gmail.com>
2019-01-10 16:26:51 +0100

    inlogForm asl eerste Form gezet

--- Match-EventApp/Match-EventApp/Program.cs
+++ Match-EventApp/Match-EventApp/Program.cs
@@ -17,7 +17,7 @@ namespace EventMatch
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MessageScreenForm());
+            Application.Run(new InlogForm());
         }
     }
 }

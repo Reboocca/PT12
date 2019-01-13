337bb60f68c3b243dcb585985162297d282bf31c
verkenner2011 verkenner2011 <leonvoijen@gmail.com>
2019-01-13 22:14:18 +0100

    .tinderform testbaar

--- Match-EventApp/Match-EventApp/TinderForm.Designer.cs
+++ Match-EventApp/Match-EventApp/TinderForm.Designer.cs
@@ -29,59 +29,56 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TinderForm));
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pb_ = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pb_like = new System.Windows.Forms.PictureBox();
+            this.pb_dislike = new System.Windows.Forms.PictureBox();
+            this.lbl_naam = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.BThome = new System.Windows.Forms.Button();
             this.BTclose = new System.Windows.Forms.Button();
             this.panel7 = new System.Windows.Forms.Panel();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pb_)).BeginInit();
+            this.lbl_favofest = new System.Windows.Forms.Label();
+            this.lbl_filmgenre = new System.Windows.Forms.Label();
+            this.lbl_hobbys = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_like)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_dislike)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel7.SuspendLayout();
             this.SuspendLayout();
             // 
-            // pictureBox3
+            // pb_like
             // 
-            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(178, 427);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(88, 84);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox3.TabIndex = 33;
-            this.pictureBox3.TabStop = false;
+            this.pb_like.BackColor = System.Drawing.Color.Transparent;
+            this.pb_like.Image = ((System.Drawing.Image)(resources.GetObject("pb_like.Image")));
+            this.pb_like.Location = new System.Drawing.Point(222, 467);
+            this.pb_like.Name = "pb_like";
+            this.pb_like.Size = new System.Drawing.Size(88, 84);
+            this.pb_like.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb_like.TabIndex = 33;
+            this.pb_like.TabStop = false;
+            this.pb_like.Click += new System.EventHandler(this.pb_like_Click);
             // 
-            // pb_
+            // pb_dislike
             // 
-            this.pb_.BackColor = System.Drawing.Color.Transparent;
-            this.pb_.Image = ((System.Drawing.Image)(resources.GetObject("pb_.Image")));
-            this.pb_.Location = new System.Drawing.Point(49, 427);
-            this.pb_.Name = "pb_";
-            this.pb_.Size = new System.Drawing.Size(88, 84);
-            this.pb_.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pb_.TabIndex = 32;
-            this.pb_.TabStop = false;
+            this.pb_dislike.BackColor = System.Drawing.Color.Transparent;
+            this.pb_dislike.Image = ((System.Drawing.Image)(resources.GetObject("pb_dislike.Image")));
+            this.pb_dislike.Location = new System.Drawing.Point(0, 467);
+            this.pb_dislike.Name = "pb_dislike";
+            this.pb_dislike.Size = new System.Drawing.Size(88, 84);
+            this.pb_dislike.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb_dislike.TabIndex = 32;
+            this.pb_dislike.TabStop = false;
+            this.pb_dislike.Click += new System.EventHandler(this.pb_dislike_Click);
             // 
-            // label1
+            // lbl_naam
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(49, 314);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 29);
-            this.label1.TabIndex = 31;
-            this.label1.Text = "Naam:";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(54, 346);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(194, 22);
-            this.textBox1.TabIndex = 30;
+            this.lbl_naam.AutoSize = true;
+            this.lbl_naam.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_naam.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_naam.Location = new System.Drawing.Point(49, 314);
+            this.lbl_naam.Name = "lbl_naam";
+            this.lbl_naam.Size = new System.Drawing.Size(88, 29);
+            this.lbl_naam.TabIndex = 31;
+            this.lbl_naam.Text = "Naam:";
             // 
             // pictureBox1
             // 
@@ -121,6 +118,7 @@
             this.BTclose.Size = new System.Drawing.Size(32, 27);
             this.BTclose.TabIndex = 14;
             this.BTclose.UseVisualStyleBackColor = false;
+            this.BTclose.Click += new System.EventHandler(this.BTclose_Click);
             // 
             // panel7
             // 
@@ -133,6 +131,34 @@
             this.panel7.Size = new System.Drawing.Size(310, 34);
             this.panel7.TabIndex = 35;
             // 
+            // lbl_favofest
+            // 
+            this.lbl_favofest.AutoSize = true;
+            this.lbl_favofest.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lbl_favofest.Location = new System.Drawing.Point(0, 343);
+            this.lbl_favofest.Name = "lbl_favofest";
+            this.lbl_favofest.Size = new System.Drawing.Size(119, 17);
+            this.lbl_favofest.TabIndex = 36;
+            this.lbl_favofest.Text = "Favoriete festival:";
+            // 
+            // lbl_filmgenre
+            // 
+            this.lbl_filmgenre.AutoSize = true;
+            this.lbl_filmgenre.Location = new System.Drawing.Point(0, 371);
+            this.lbl_filmgenre.Name = "lbl_filmgenre";
+            this.lbl_filmgenre.Size = new System.Drawing.Size(133, 17);
+            this.lbl_filmgenre.TabIndex = 37;
+            this.lbl_filmgenre.Text = "Favoriete filmgenre:";
+            // 
+            // lbl_hobbys
+            // 
+            this.lbl_hobbys.AutoSize = true;
+            this.lbl_hobbys.Location = new System.Drawing.Point(0, 398);
+            this.lbl_hobbys.Name = "lbl_hobbys";
+            this.lbl_hobbys.Size = new System.Drawing.Size(63, 17);
+            this.lbl_hobbys.TabIndex = 38;
+            this.lbl_hobbys.Text = "Hobby\'s:";
+            // 
             // TinderForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -140,18 +166,20 @@
             this.BackgroundImage = global::Match_EventApp.Properties.Resources.Achtergrond1;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(310, 550);
+            this.Controls.Add(this.lbl_hobbys);
+            this.Controls.Add(this.lbl_filmgenre);
+            this.Controls.Add(this.lbl_favofest);
             this.Controls.Add(this.panel7);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pb_);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbl_naam);
+            this.Controls.Add(this.pb_like);
+            this.Controls.Add(this.pb_dislike);
             this.Controls.Add(this.pictureBox1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "TinderForm";
             this.Text = "Form3";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pb_)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_like)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_dislike)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel7.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -160,13 +188,15 @@
         }
 
         #endregion
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pb_;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pb_like;
+        private System.Windows.Forms.PictureBox pb_dislike;
+        private System.Windows.Forms.Label lbl_naam;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button BThome;
         private System.Windows.Forms.Button BTclose;
         private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Label lbl_favofest;
+        private System.Windows.Forms.Label lbl_filmgenre;
+        private System.Windows.Forms.Label lbl_hobbys;
     }
 }
--- Match-EventApp/Match-EventApp/TinderForm.cs
+++ Match-EventApp/Match-EventApp/TinderForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Match_EventApp.Classes;
+using Match_EventApp.CustomRenderer;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,35 +14,53 @@ namespace Match_EventApp
 {
     public partial class TinderForm : Form
     {
+        private Random rnd = new Random();
+        private Database dbs = new Database();
+        private Account acc = new Account();
         public TinderForm()
         {
             InitializeComponent();
+            Showprofile();
         }
 
         public void Like()
         {
-
+            dbs.insertlike(acc._idAccount, Showprofile());
         }
 
         public void Dislike()
         {
+            dbs.insertdislike(acc._idAccount, Showprofile());
+        }
+
+        public int Showprofile()
+        {
+            int rndint = rnd.Next(0, 6);
+            Profiel profiel = new Profiel();
+            profiel = dbs.getProfiel()[rndint];
+
+            lbl_naam.Text = profiel._voornaam + profiel._achternaam;
+            lbl_favofest.Text += profiel._favofestival;
+            lbl_filmgenre.Text += profiel._favofilms;
+            lbl_hobbys.Text += profiel._hobbys;
+
+            return rndint;
 
         }
 
-        public void Superlike()
+        private void pb_dislike_Click(object sender, EventArgs e)
         {
-
+            Dislike();
         }
 
-        public void Showprofileimg()
+        private void pb_like_Click(object sender, EventArgs e)
         {
-
-
+            Like();
         }
 
-        public void Selectprofile()
+        private void BTclose_Click(object sender, EventArgs e)
         {
-
+            Hide();
         }
     }
 }
